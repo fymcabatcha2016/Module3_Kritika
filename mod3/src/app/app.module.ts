8878d80415c7016b6e8a0959686708704102f1e5
Kritika <fymcabatcha2016@users.noreply.github.com>
2018-07-30 03:20:35 +0530

    recepie and shooping list component created

--- mod3/src/app/app.module.ts
+++ mod3/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ReceipeComponent } from './receipe/receipe.component';
+import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ReceipeComponent,
+    ShoppinglistComponent
   ],
   imports: [
     BrowserModule
