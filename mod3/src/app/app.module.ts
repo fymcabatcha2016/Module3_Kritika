ba8737fb198beb0317f62404be15aa7a50f328d0
Kritika <fymcabatcha2016@users.noreply.github.com>
2018-07-30 03:28:57 +0530

    Recepie Hireachy Component Cretated
    Recepie - Detail and list
    list - Item

--- mod3/src/app/app.module.ts
+++ mod3/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ReceipeComponent } from './receipe/receipe.component';
 import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
+import { RecepieListComponent } from './receipe/recepie-list/recepie-list.component';
+import { RecepieDetailComponent } from './receipe/recepie-detail/recepie-detail.component';
+import { RecepieItemComponent } from './receipe/recepie-list/recepie-item/recepie-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ReceipeComponent,
-    ShoppinglistComponent
+    ShoppinglistComponent,
+    RecepieListComponent,
+    RecepieDetailComponent,
+    RecepieItemComponent
   ],
   imports: [
     BrowserModule
