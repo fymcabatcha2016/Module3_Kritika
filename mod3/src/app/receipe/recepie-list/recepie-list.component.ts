41439719e8084ef2de1543ec1833942c543de3aa
Kritika <fymcabatcha2016@users.noreply.github.com>
2018-08-07 03:23:27 +0530

    Image Size

--- mod3/src/app/receipe/recepie-list/recepie-list.component.ts
+++ mod3/src/app/receipe/recepie-list/recepie-list.component.ts
@@ -9,7 +9,7 @@ import { Recipie } from './recepie-item/Recipie';
 export class RecepieListComponent implements OnInit {
   recipies:Recipie[]=[
     new Recipie("Vada pav","bun and fried paties","/assets/vadapav.jpg"),
-    new Recipie("Vada pav","bun and fried paties","/assets/vadapav.jpg"),
+    new Recipie("Samosa Pav","bun and fried paties","/assets/vadapav.jpg"),
     new Recipie("Vada pav","bun and fried paties","/assets/vadapav.jpg")
 
   ];
